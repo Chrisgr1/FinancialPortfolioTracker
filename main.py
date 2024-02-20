42a0c3d258058a78002d351731628025356d9184
Chris Skinner <95317377+Chrisgr1@users.noreply.github.com>
2024-02-20 15:40:04 +0000

    got api of top 25 companies, presented as dictionary - needs structure

--- main.py
+++ main.py
@@ -1,14 +1,51 @@
 from flask import Flask, render_template
+import yfinance as yf
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def home():
-    # Get the apple data using yfinance
-    import yfinance as yf
-    apple = yf.Ticker("aapl")
-    info = apple.info
-    return render_template('home.html', apple_info=info)
+    # # Get the apple data using yfinance
+    # apple = yf.Ticker("aapl")
+    # info = apple.info
+    # return render_template('home.html', apple_info=info)
+    companies = (
+('Shell', 'SHEL'),
+('AstraZeneca', 'AZN'),
+('HSBC Holdings', 'HSBA'),
+('BHP Group', 'BHP'),
+('Unilever', 'ULVR'),
+('Rio Tinto', 'RIO'),
+('GlaxoSmithKline', 'GSK'),
+('Diageo', 'DGE'),
+('RELX', 'REL'),
+('British American Tobacco', 'BATS'),
+('Glencore', 'GLEN'),
+('London Stock Exchange Group', 'LSEG'),
+('Reckitt Benckiser Group', 'RB'),
+('BAE Systems', 'BA'),
+('Compass Group', 'CPG'),
+('National Grid', 'NG'),
+('Experian', 'EXPN'),
+('Flutter Entertainment', 'FLTR'),
+('Haleon', 'HLN'),
+('Lloyds Banking Group', 'LLOY'),
+('Rolls-Royce Group', 'RR'),
+('Anglo American', 'AAL'),
+('3i Group', 'III'),
+('Ashtead Group', 'AHT'),
+('Prudential', 'PRU')
+)
+
+    companies_info = []
+    for company, symbol in companies:
+        ticker = yf.Ticker(symbol)
+        info = ticker.info
+        companies_info.append({'name': company, 'info': info})
+
+    return render_template('home.html', companies_info=companies_info)
 
 @app.route('/company/<symbol>')
 def company(symbol):
